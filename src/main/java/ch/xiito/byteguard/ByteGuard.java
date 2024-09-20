aafd6803577ec855dd4e3face5d5b9979c50360b
Xiito <celyan@stayfitandhappy.ch>
2024-09-20 18:05:28 +0200

    v1.0

---
+++ src/main/java/ch/xiito/byteguard/ByteGuard.java
@@ -0,0 +1,33 @@
+package ch.xiito.byteguard;
+
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
+public class ByteGuard {
+
+    private final ShardManager shardManager;
+
+    public ByteGuard() throws LoginException {
+        String token = "MTI4MDQ1NTU0ODc4OTMyNTg4OA.GcLzcY.c8VJWBn-o49SU0Ju5TaKvpP6gnPiMeR3qoEIiA";
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.customStatus("Coding Myself"));
+        shardManager = builder.build();
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    public static void main(String[] args) {
+        try {
+            ByteGuard bot = new ByteGuard();
+        } catch (LoginException e) {
+            System.out.println("ERROR: Provided bot token is invalid!");
+        }
+    }
+}
